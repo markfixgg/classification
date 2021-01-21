f48a435340a1eb8041d865be00bca71b4b430742
markfixgg <lebedenko077@gmail.com>
2021-01-21 20:00:01 +0300

    Refactor

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-const tf = require('@tensorflow/tfjs');
-const mobilenet = require('@tensorflow-models/mobilenet');
-const tfnode = require('@tensorflow/tfjs-node');
-
+// Main module
 const myUtils = require("./modules/myUtils")
-var path = require('path');
-const sharp = require('sharp');
 
+// Express initial
 const express = require('express')
 const cors = require('cors');
 const util = require("util");
-const fs = require('fs')
 const bodyParser = require("body-parser");
 
+// Express config
 const app = express()
 app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}))
 app.use(bodyParser.json())
 app.use(cors({ origin: '*', credentials :  true}))
 
 
-app.post("/classify", async (req, res)=>{
-    var test = myUtils.classify(req.body.base64, res);
-})
+app.post("/classify", async (req, res) => myUtils.classify(req.body.base64, res)) // main route
+
 
 app.listen(80, () => {console.log(`App started!`)});
